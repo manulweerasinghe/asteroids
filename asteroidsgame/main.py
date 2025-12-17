3c8e5c053f5601cb089b8ea438bf466d60e88e5c
manulweerasinghe <weerasinghe@gmail.com>
2025-12-17 18:12:14 +0530

    add player

--- asteroidsgame/main.py
+++ asteroidsgame/main.py
@@ -1,19 +1,30 @@
 import pygame
 from logger import log_state
 from constants import *
+from player import Player
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        pause = clock.tick(60)
+        dt = pause/1000
 
 if __name__ == "__main__":
     main()
